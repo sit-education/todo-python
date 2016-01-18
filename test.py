a3d9958685e961ce34cc11d1c288e21732be6abf
Shah_rustam <shahrustam98@gmail.com>
2016-01-18 14:08:28 +0200

    TEST API without signup & restore password (fixed)
    1)test login GET
    2)test items POST
    3)test item(add) POST
    4)test item/ PUT
    5)test item/ DELETE
    5)test logout POST

--- test.py
+++ test.py
@@ -4,54 +4,57 @@ import unittest
 
 class TestServerFunctionality(unittest.TestCase):
 
-	def setUp(self):
+	def setUp(self):  #setUp function is called before each test, a built-in method of unittest
 		self.url= 'http://ec2-52-11-248-121.us-west-2.compute.amazonaws.com/api/v1'
 		self.loginval= {"login":"test", "password":"test"}
 
-	def test_1Login(self):
-		requestLogin = requests.post(self.url+'/login', json= self.loginval)
-		Token=(requestLogin.json().get('data')).get('tokenKey')
+	def test_1_login(self):
+		requestLogin = requests.post(self.url+'/login', json = self.loginval)
+		token = (requestLogin.json().get('data')).get('tokenKey')
 		self.assertEqual(requestLogin.json().get('status'), 1)
-		EmailVerif= (requestLogin.json().get('data')).get('emailVerified')
-		self.assertEqual(EmailVerif,1)
-		userid= (requestLogin.json().get('data')).get('user_id')
-		self.assertEqual(EmailVerif,1)
-		return Token
+		emailVerif = (requestLogin.json().get('data')).get('emailVerified')
+		self.assertEqual(emailVerif,1)
+		userid = (requestLogin.json().get('data')).get('user_id')
+		self.assertEqual(userid,1)
 
-	def test_2Items(self):
-		Token= self.test_1Login()
-		headers={'Token-Key':'%s'%Token }
-		requestItems = requests.get(self.url+'/items', headers=headers)
+	def test_2_get_items(self):
+		requestLogin = requests.post(self.url+'/login', json = self.loginval)
+		token = (requestLogin.json().get('data')).get('tokenKey')
+		headers = {'Token-Key':'%s'%token }
+		requestItems = requests.get(self.url+'/items', headers = headers)
 		self.assertEqual(requestItems.json().get('status'), 1)
-		todoData= (requestItems.json().get('data')).get('todoData')
+		todoData = (requestItems.json().get('data')).get('todoData')
 		for x in range(0,len(todoData)):
 			self.assertEqual(todoData[x].get('description'),'test%s'%(x+1))
 			self.assertEqual(todoData[x].get('title'),'test%s'%(x+1))
 			self.assertEqual(todoData[x].get('id'),(x+1))
 
-
-	def test_3ItemAdd(self):
-		Token= self.test_1Login()
-		headers={'Token-Key':'%s'%Token }
-		requestItem = requests.post(self.url+'/item', json= {"title":"test4", "description":"test4"}, headers=headers)
+	def test_3_add_item(self):
+		requestLogin = requests.post(self.url+'/login', json = self.loginval)
+		token = (requestLogin.json().get('data')).get('tokenKey')
+		headers = {'Token-Key':'%s'%token }
+		requestItem = requests.post(self.url+'/item', json = {"title":"test4", "description":"test4"}, headers = headers)
 		self.assertEqual(requestItem.json().get('status'), 1)
 
-	def test_4ItemEdit(self):
-		Token= self.test_1Login()
-		headers={'Token-Key':'%s'%Token }
-		requestItemEdit = requests.put(self.url+'/item/4', json= {"title":"test4", "description":"test4"}, headers=headers)
+	def test_4_edit_item(self):
+		requestLogin = requests.post(self.url+'/login', json = self.loginval)
+		token = (requestLogin.json().get('data')).get('tokenKey')
+		headers = {'Token-Key':'%s'%token }
+		requestItemEdit = requests.put(self.url+'/item/4', json = {"title":"test4", "description":"test4"}, headers = headers)
 		self.assertEqual(requestItemEdit.json().get('status'), 1)
 
-	def test_5DeleteItem(self):
-		Token= self.test_1Login()
-		headers={'Token-Key':'%s'%Token }
-		requestItemDelete = requests.delete(self.url+'/item/4', headers=headers)
+	def test_5_delete_item(self):
+		requestLogin = requests.post(self.url+'/login', json = self.loginval)
+		token = (requestLogin.json().get('data')).get('tokenKey')
+		headers = {'Token-Key':'%s'%token }
+		requestItemDelete = requests.delete(self.url+'/item/4', headers = headers)
 		self.assertEqual(requestItemDelete.json().get('status'), 1)
 
-	def test_6Logout(self):
-		Token= self.test_1Login()
-		headers={'Token-Key':'%s'%Token }
-		requestLogout=requests.post(self.url+'/logout', headers=headers)
+	def test_6_logout(self):
+		requestLogin = requests.post(self.url+'/login', json = self.loginval)
+		token = (requestLogin.json().get('data')).get('tokenKey')
+		headers = {'Token-Key':'%s'%token }
+		requestLogout = requests.post(self.url+'/logout', headers = headers)
 		self.assertEqual(requestLogout.json().get('status'), 1)
 
 if __name__ == '__main__':
